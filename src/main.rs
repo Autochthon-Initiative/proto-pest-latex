892ea147bc4070b78074723b6374d59f36f0a614
Wak91 <mhlaca@kpmg.it>
2020-10-09 15:36:51 +0200

    Added some rules

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,10 @@ const EXAMPLE_FORMULA_1: &'static str = "E = mc^2";
 #[allow(dead_code)]
 const EXAMPLE_FORMULA_2: &'static str = "e^{i \\pi} + 1 = 0"; // The Euler Identity
 
+#[allow(dead_code)]
+const EXAMPLE_FORMULA_3: &'static str = "\\sum {i=1}^N (y_i – X_i \\cdot \\beta)^2"; // Latex formula from Word
+
 fn main() {
-    let parsed = LaTeXFormulaParser::parse(Rule::formula, EXAMPLE_FORMULA_1).unwrap();
+    let parsed = LaTeXFormulaParser::parse(Rule::expression, EXAMPLE_FORMULA_3).unwrap();
     println!("{:#?}", parsed);
 }
